6a0f0bba35455d031bcb98e2a2c775f6e136fabb
HarmfulPerson <marcinww@op.pl>
2020-03-20 15:25:03 +0100

    Added navigation

--- App.js
+++ App.js
@@ -1,15 +1,35 @@
-import React from 'react';
-import { StyleSheet, Text, View} from 'react-native';
-import RegisterFormView from './screens/Register';
-import LoginFormView from './screens/Login';
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import { StyleSheet, Text, View} from 'react-native';import 'react-native-gesture-handler';
+import { StyleSheet } from 'react-native';
+import {createStackNavigator} from "react-navigation-stack";
+import Home from "./screens/Home";
+import Login from "./screens/Login";
+import {createAppContainer} from "react-navigation";
+import Register from "./screens/Register";
+import NotificationBoard from "./screens/notificationBoard";
+import Contact from "./screens/Contact";
+import TypesOfPrevention from "./screens/typesOfPrevention";
+const RootStack = createStackNavigator(
+    {
+      Home: {screen: Home},
+      Login:{screen:Login},
+      Register: {screen: Register},
+      NotificationBoard:{screen: NotificationBoard},
+      Contact: {screen: Contact},
+      TypesOfPrevention: {screen: TypesOfPrevention}
 
+    },{
+      initialRouteName: 'Home'
+    }
+);const AppContainer = createAppContainer(RootStack);
 
-export default function App() {
-  return (
-    <View>
-      <RegisterFormView/>
-    </View>
-  );
+export default class App extends React.Component{
+  render() {
+    return (
+        <AppContainer/>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
