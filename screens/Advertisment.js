0ced99adc450800858bfd76346a733fe20641853
Marcin Brzóska <mbrzoska303@gmail.com>
2020-03-21 11:48:32 +0100

    Add css style and icons

--- screens/Advertisment.js
+++ screens/Advertisment.js
@@ -1,7 +1,8 @@
 import * as React from 'react';
 import { Image, Platform, StyleSheet, Text, TouchableOpacity, View, NativeModules, TextInput, Button, Keyboard } from 'react-native';
 import { ScrollView, RectButton } from 'react-native-gesture-handler';
-
+import colors from "../consts/colors";
+import fonts from "../consts/fontSize";
 
 
 
@@ -27,24 +28,28 @@ export default class AdvertisementView extends React.Component{
 
     needHelp(){
         if(this.state.advertisement.needHelp==true){
-            return "need your help!! :)";
-        } else return "wanna help you!! :)"
+            return " need your help!! :)";
+        } else return " wanna help you!! :)"
     }
 
     render(){
         return(
-
-            <View style={styles.container}>
-                <View style={styles.top_bar}>
-        <Text style={styles.needHelpText}>{this.state.advertisement.user}{this.needHelp()}</Text>
+            <ScrollView>
+                <View style={styles.container}>
+                    <View style={styles.container}>
+                        <View style={styles.top_bar}>
+                            <Text style={styles.needHelpText}>{this.state.advertisement.user}{this.needHelp()}</Text>
+                        </View>
+                        <View style={styles.localization_date}>
+                            <View><Text style={styles.localization_date_text}>{this.state.advertisement.localization}</Text></View>
+                            <View><Text style={styles.localization_date_text}>{this.state.advertisement.date}</Text></View>
+                        </View>
+                        <View style={styles.title}><Text style={styles.title_text}>{this.state.advertisement.title}</Text></View>
+                        <View style={styles.description}><Text style={styles.description_text}>{this.state.advertisement.description}</Text></View>
+                    </View>
                 </View>
-                <View style={styles.localization_date}>
-                    <View><Text>{this.state.advertisement.localization}</Text></View>
-                    <View><Text>{this.state.advertisement.date}</Text></View>
-                </View>
-                <View style={styles.title}><Text style={{fontSize:20}}>{this.state.advertisement.title}</Text></View>
-                <View style={styles.description}><Text>{this.state.advertisement.description}</Text></View>
-            </View>
+            </ScrollView>
+            
         );
     }
 
@@ -52,26 +57,30 @@ export default class AdvertisementView extends React.Component{
 
 const styles = StyleSheet.create({
     container: {
-        flex: 0.9,
-        backgroundColor: '#C4E2FF',
-        marginTop: 40,
+        flex: 1,
+        backgroundColor: colors.main,
+        marginTop: 20,
         marginHorizontal: 10
 
     },
     top_bar:{
-        flex: 0.2,
-        backgroundColor: '#99CCFF',
+        flex: 1,
+        backgroundColor: colors.second,
         alignItems: "center",
-        justifyContent: 'center'
+        justifyContent: 'center',
+        textAlign:"center",
     },
     needHelpText:{
-        fontSize: 25
+        fontSize: fonts.h1
     },
     localization_date:{
         alignItems: 'flex-end',
         marginRight: 5,
         marginTop: 5,
         marginBottom: 40
+    },
+    localization_date_text:{
+        fontSize: fonts.h4,
     },
     title:{
         alignItems:"center",
@@ -82,5 +91,11 @@ const styles = StyleSheet.create({
         alignItems:"center",
         marginHorizontal: 25,
         marginVertical: 30
+    },
+    description_text:{
+        fontSize: fonts.h3,
+    },
+    title_text:{
+        fontSize: fonts.h2,
     }
 })
